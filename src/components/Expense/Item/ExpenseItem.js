f4a5ec1e34d5c3a44ff68b497b6eb388c1a16657
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-02-21 10:26:12 +0330

    delete change title button

--- src/components/Expense/Item/ExpenseItem.js
+++ src/components/Expense/Item/ExpenseItem.js
@@ -4,18 +4,17 @@ import ExpenseDate from "../Date/ExpenseDate";
 import Card from "../../UI/Card/card";
 
 function Expense({ expense }) {
-  const [title, setTitle] = useState(expense.title);
-
-  const clickHandler = () => setTitle("update");
+  // const [title, setTitle] = useState(expense.title);
+  // const clickHandler = () => setTitle("update");
 
   return (
     <Card className="expense-item">
       <ExpenseDate props={expense.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{expense.title}</h2>
         <div className="expense-item__price">${expense.amount}</div>
       </div>
-      <button onClick={clickHandler}>change title</button>
+      {/* <button onClick={clickHandler}>change title</button> */}
     </Card>
   );
 }
