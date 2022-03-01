97ea0a4908e98fd96cc1a4f8e523e7616c22c66f
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-03-01 16:55:58 +0330

    add loading state

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,11 +1,36 @@
-import Card from '../UI/Card';
-import MealItem from './MealItem/MealItem';
-import classes from './AvailableMeals.module.css';
+import { useState, useEffect } from "react";
 
-const
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [dummyMeals, setDummyMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchMealsData = async () => {
+      const respond = await (
+        await fetch("http://localhost:8000/DUMMY_MEALS")
+      ).json();
+
+      const getData = respond;
+      getData.map((data) => (data.price = +data.price));
+      setDummyMeals(getData);
+      setIsLoading(false);
+    };
+    fetchMealsData();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  const mealsList = dummyMeals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,20 +1,21 @@
-import { useContext } from 'react';
+import { useContext } from "react";
 
-import MealItemForm from './MealItemForm';
-import classes from './MealItem.module.css';
-import CartContext from '../../../store/cart-context';
+import MealItemForm from "./MealItemForm";
+import classes from "./MealItem.module.css";
+import CartContext from "../../../store/cart-context";
 
 const MealItem = (props) => {
   const cartCtx = useContext(CartContext);
 
+  console.log(props.price);
   const price = `$${props.price.toFixed(2)}`;
 
-  const addToCartHandler = amount => {
+  const addToCartHandler = (amount) => {
     cartCtx.addItem({
       id: props.id,
       name: props.name,
       amount: amount,
-      price: props.price
+      price: props.price,
     });
   };
 
