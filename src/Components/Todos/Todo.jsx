bcb71cd267ef82a4f353a98ac121c5d59251f88c
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-03-23 18:51:32 +0430

    handler remove task

--- src/Components/Todos/Todo.jsx
+++ src/Components/Todos/Todo.jsx
@@ -5,19 +5,22 @@ import removeImage from "../../assets/icon-cross.svg";
 
 const Todo = () => {
   const tasks = useContext(TodoCtx).tasks;
+  const removeTodo = useContext(TodoCtx).removeTask;
   const checkTaskIsEmpty = tasks.length === 0;
 
+  //remove todo handling
+  const removeTodoHandler = (id) => removeTodo(id);
 
   const mapTask = tasks.map((task) => (
-    <li className={classes["todo-item"]}>
+    <li className={classes["todo-item"]} key={task.id}>
       <input type="checkbox" name="complete" />
       <p>{task.name}</p>
-      <button>
+      <button onClick={() => removeTodoHandler(task.id)}>
         <img src={removeImage} alt="Delete task button" />
       </button>
     </li>
   ));
-  
+
   const massageEmptyTask = (
     <li className={classes.massage}>Tasks list is empty!</li>
   );
