73b8f77f1b82665e70ab0b3a12c1f1d14ba6e575
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-03-23 18:55:29 +0430

    clear all todos handler

--- src/Components/Todos/TodoINfo.jsx
+++ src/Components/Todos/TodoINfo.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import classes from "./TodoInfo.module.css";
 import TextBtn from "../TextBtn/TextBtn";
+import TodoCtx from "../../store/task-context";
 
 const TodoInfo = () => {
+  const clearAllTodos = useContext(TodoCtx).clearAllTask;
+
+  const clearAllTodosHandler = () => clearAllTodos();
+
   return (
     <section className={classes["todo-list-info"]}>
       <p>
         <span>0</span> items left
       </p>
-      <TextBtn>Clear Completed</TextBtn>
+      <TextBtn onClick={clearAllTodosHandler}>Clear Completed</TextBtn>
     </section>
   );
 };
