e83ec3225beacba9cfc9c5646c0ff18be5f1bb62
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-03-23 14:59:03 +0430

    add drag drap text

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import TodoWrapper from "../Components/Todos/TodoWrapper";
 import FilterSection from "../Components/FilterTodo/FilterSection";
+import TextBtn from "../Components/TextBtn/TextBtn";
 
 const Main = () => {
   return (
-    <main>
+    <main style={{ textAlign: "center" }}>
       <TodoWrapper />
       <FilterSection />
+      <TextBtn class="mt5">Drag and drop to reorder list</TextBtn>
     </main>
   );
 };
